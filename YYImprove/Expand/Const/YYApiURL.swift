6f9a2a2680425d88f0022ed3f2e5f0ecdfd9ef5f
yangyu <yang_yu92@foxmail.com>
2018-04-28 20:31:31 +0800

    修改项目目录结构

---
+++ YYImprove/Expand/Const/YYApiURL.swift
@@ -0,0 +1,9 @@
+//
+//  YYApiURL.swift
+//  YYImprove
+//
+//  Created by canyou on 2018/4/28.
+//  Copyright © 2018年 com.canyou. All rights reserved.
+//
+
+import Foundation
---
+++ YYImprove/Expand/Const/YYConstants.swift
@@ -0,0 +1,13 @@
+//
+//  YYConstants.swift
+//  YYImprove
+//
+//  Created by canyou on 2018/4/28.
+//  Copyright © 2018年 com.canyou. All rights reserved.
+//
+
+import Foundation
+
+public let kSCREEN_WIDTH: CGFloat = UIScreen.main.bounds.size.width
+public let kSCREEN_HEIGHT: CGFloat = UIScreen.main.bounds.size.height
+public let kScreen_Bounds: CGRect = UIScreen.main.bounds
