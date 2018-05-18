f0b22fc4fdc4d0f807be2c2ee0626b1bfb9d3256
yangyu <yang_yu92@foxmail.com>
2018-05-18 10:56:08 +0800

    XCGLogger日志管理组件调整到单独文件中初始化

---
+++ YYImprove/AppDelegate/AppDelegate+XCGLogger.swift
@@ -0,0 +1,34 @@
+//
+//  AppDelegate+XCGLog.swift
+//  YYImprove
+//
+//  Created by canyou on 2018/5/18.
+//  Copyright © 2018年 com.canyou. All rights reserved.
+//
+
+import Foundation
+import XCGLogger
+
+let log = XCGLogger.default
+
+extension AppDelegate {
+    func setupXCGLogger() {
+        log.remove(destinationWithIdentifier: XCGLogger.Constants.baseConsoleDestinationIdentifier)
+        log.add(destination: AppleSystemLogDestination(identifier: XCGLogger.Constants.systemLogDestinationIdentifier))
+
+        let urls = FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask)
+        // Create a file log destination
+        let logPath: URL = urls[urls.endIndex - 1].appendingPathComponent("XCGLogger_Log.txt")
+        let autoRotatingFileDestination = AutoRotatingFileDestination(writeToFile: logPath,
+                                                                      identifier: "advancedLogger.fileDestination",
+                                                                      shouldAppend: true,
+                                                                      attributes: [.protectionKey: FileProtectionType.completeUntilFirstUserAuthentication],
+                                                                      maxFileSize: 1024 * 5,
+                                                                      maxTimeInterval: 60)
+        autoRotatingFileDestination.targetMaxLogFiles = 10
+        autoRotatingFileDestination.logQueue = XCGLogger.logQueue
+        log.add(destination: autoRotatingFileDestination)
+        log.logAppDetails()
+    }
+    
+}
