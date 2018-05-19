a1cf092b0e5c7f22dd8508d989434b4eb46056e4
yangyu <yang_yu92@foxmail.com>
2018-05-19 14:27:54 +0800

    使用SwiftGen统一管理图片资源与字符串

--- YYImprove/Main/Public/YYRootTabBarController.swift
+++ YYImprove/Main/Public/YYRootTabBarController.swift
@@ -29,20 +29,32 @@ class YYRootTabBarController: ESTabBarController {
         let controller3 = ViewController()
         let controller4 = ViewController()
         
-        controller1.tabBarItem = ESTabBarItem.init(AnimationTabBarView(), title: "首页", image: UIImage(named: "home"), selectedImage: UIImage(named: "home_1"))
-        controller2.tabBarItem = ESTabBarItem.init(AnimationTabBarView(), title: "文章", image: UIImage(named: "photo"), selectedImage: UIImage(named: "photo_1"))
-        controller3.tabBarItem = ESTabBarItem.init(AnimationTabBarView(), title: "发现", image: UIImage(named: "favor"), selectedImage: UIImage(named: "favor_1"))
-        controller4.tabBarItem = ESTabBarItem.init(AnimationTabBarView(), title: "我的", image: UIImage(named: "me"), selectedImage: UIImage(named: "me_1"))
+        controller1.tabBarItem = ESTabBarItem.init(AnimationTabBarView(),
+                                                   title: L10n.tabTitleHome,
+                                                   image: UIImage(asset: Asset.Navication.tabHome),
+                                                   selectedImage: UIImage(asset: Asset.Navication.tabHomeSelected))
+        controller2.tabBarItem = ESTabBarItem.init(AnimationTabBarView(),
+                                                   title: L10n.tabTitlePhoto,
+                                                   image: UIImage(asset: Asset.Navication.tabPhoto),
+                                                   selectedImage: UIImage(asset: Asset.Navication.tabPhotoSelected))
+        controller3.tabBarItem = ESTabBarItem.init(AnimationTabBarView(),
+                                                   title: L10n.tabTitleFavor,
+                                                   image: UIImage(asset: Asset.Navication.tabFavor),
+                                                   selectedImage: UIImage(asset: Asset.Navication.tabFavorSelected))
+        controller4.tabBarItem = ESTabBarItem.init(AnimationTabBarView(),
+                                                   title: L10n.tabTitleMe,
+                                                   image: UIImage(asset: Asset.Navication.tabMe),
+                                                   selectedImage: UIImage(asset: Asset.Navication.tabMeSelected))
         
         let navigation1 = YYBaseNavigationController.init(rootViewController: controller1)
         let navigation2 = YYBaseNavigationController.init(rootViewController: controller2)
         let navigation3 = YYBaseNavigationController.init(rootViewController: controller3)
         let navigation4 = YYBaseNavigationController.init(rootViewController: controller4)
         
-        controller1.title = "首页"
-        controller2.title = "文章"
-        controller3.title = "发现"
-        controller4.title = "我的"
+        controller1.title = L10n.tabTitleHome
+        controller2.title = L10n.tabTitlePhoto
+        controller3.title = L10n.tabTitleFavor
+        controller4.title = L10n.tabTitleMe
         
         self.viewControllers = [navigation1, navigation2, navigation3, navigation4]
         
